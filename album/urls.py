a8c3b4e204358f7841236712f33106630737a1f1
fuad manye / <fahadbasi333@gmail.com>
2022-06-01 12:15:52 +0300

    add paragrapgs in display.html

--- album/urls.py
+++ album/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
   
-    path('images/',views.index, name = 'home'),
-    path('',views.landing, name = 'landing'),
+    path('',views.index, name = 'home'),
+    path('landing',views.landing, name = 'landing'),
     path('nature', views.nature_images, name = "nature"),
     path('park', views.park_images, name = "park"),
     path('beach', views.beach_images, name = "beach"),
